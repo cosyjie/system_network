c4e88aaebdcf3c2979fdf4b097dbf97d22710ebb
IT阿杰 <107783255+cosyjie@users.noreply.github.com>
2024-07-17 16:33:09 +0800

    -

--- menu.py
+++ menu.py
@@ -1,12 +1,10 @@
-from django.urls import reverse_lazy
-
 menu = {
     'module_system': {
         'child': [
             {
                 'name': 'system_network',
                 'title': '网络配置',
-                'href': reverse_lazy('module_system:system_network:device_list'),
+                'href': 'module_system:system_network:device_list',
             },
         ]
     }
